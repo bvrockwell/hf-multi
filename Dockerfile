b873c23f6f98322fa8e4de7e83e1b356352bf750
bvrockwell <rockwellb@google.com>
2024-08-01 17:15:36 -0700

    add command

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,10 @@ FROM us-east5-docker.pkg.dev/google.com/vertex-training-dlexamples/nemo-sd-train
 
 WORKDIR /
 
+# export MODEL_NAME="stabilityai/stable-diffusion-3-medium-diffusers"
+ENV MODEL_NAME="/gcs/dlexamples-shared-data/sd3-dreambooth/models--stabilityai--stable-diffusion-3-medium-diffusers"
+ENV INSTANCE_DIR="/gcs/dlexamples-shared-data/sd3-dreambooth/dog"
+ENV OUTPUT_DIR="/tmp/sd3-output"
+
 CMD ["/bin/bash", "-c", "git clone https://github.com/bvrockwell/hf-multi.git", "./hf-multi/set_env.sh", "./opt/conda/bin/accelerate config update --config_file $ACC_CONFIG", "$LAUNCH_CMD"]
 
--- set_env.sh
+++ set_env.sh
@@ -31,10 +31,6 @@ export MASTER_ADDR=$(if [[ $RANK -gt 0 ]]; then echo $MASTER_ADDR;else echo loca
 export MASTER_PORT=$MASTER_PORT
 export NUM_PROCESSES=$(($NODE_COUNT * $NUM_PROCESS))
 
-export MODEL_NAME="stabilityai/stable-diffusion-3-medium-diffusers"
-export MODEL_NAME="/gcs/dlexamples-shared-data/sd3-dreambooth/models--stabilityai--stable-diffusion-3-medium-diffusers"
-export INSTANCE_DIR="/gcs/dlexamples-shared-data/sd3-dreambooth/dog"
-export OUTPUT_DIR="/tmp/sd3-output"
 export ACC_CONFIG="${ACC_CONFIG:-/hf-multi/2host_config.yaml}"
 
 export LAUNCHER="./opt/conda/bin/accelerate launch \
