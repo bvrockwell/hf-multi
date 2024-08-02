e4e1ab8ce70135a176fd9f8747dfcb0b6af1bee9
bvrockwell <rockwellb@google.com>
2024-08-01 17:44:50 -0700

    fix

--- set_env.sh
+++ set_env.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 export PYTHONPATH="./"
 export NCCL_LIB_DIR="/usr/local/nvidia/lib64"
@@ -27,7 +27,7 @@ export NCCL_FASTRAK_PLUGIN_ACCEPT_TIMEOUT_MS=600000
 export NCCL_NVLS_ENABLE=0 
 
 export NCCL_FASTRAK_PLUGIN_ACCEPT_TIMEOUT_MS=$NCCL_FASTRAK_PLUGIN_ACCEPT_TIMEOUT_MS
-export MASTER_ADDR=$(if [[ $RANK -gt 0 ]]; then echo $MASTER_ADDR;else echo localhost;fi)
+export MASTER_ADDR=$(if [[ $RANK=0 ]]; then echo $MASTER_ADDR;else echo localhost;fi)
 export MASTER_PORT=$MASTER_PORT
 export NUM_PROCESSES=$(($NODE_COUNT * $NUM_PROCESS))
 
